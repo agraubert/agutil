bf3bb37f52b3207e50fb1dfcf8efc53990f58b71
agraubert <captianjroot@live.com>
2016-06-22 13:24:28 -0500

    Improved testing of status bar by checking percentage indicator

--- tests/test_status_bar.py
+++ tests/test_status_bar.py
@@ -29,6 +29,7 @@ class test(unittest.TestCase):
         self.assertEqual(q.display, '['+(' '*q.cols)+'] 0.000%')
         threshold = 10000/q.cols
         self.assertEqual(q.threshold, threshold)
+        self.assertEqual(q.update_threshold, .05 * threshold)
         for i in range(10000):
             q.update(i)
             num = int(i*q.cols/10000.0)
@@ -37,6 +38,7 @@ class test(unittest.TestCase):
             for j in range(q.cols):
                 self.assertEqual(q.display[j+1], '=' if j < num else ' ')
             self.assertEqual(q.display[0], '[')
+            self.assertLessEqual(abs(float(q.display.split()[-1].strip('% '))- (100*i/10000)), q.update_threshold)
         for i in range(10000, 0, -1):
             q.update(i)
             num = int(i*q.cols/10000.0)
@@ -45,3 +47,4 @@ class test(unittest.TestCase):
             for j in range(q.cols):
                 self.assertEqual(q.display[j+1], '=' if j < num else ' ')
             self.assertEqual(q.display[0], '[')
+            self.assertLessEqual(abs(float(q.display.split()[-1].strip('% '))- (100*i/10000)), q.update_threshold)
