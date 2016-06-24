bd9bb09abcffed25b13801950d2c224860f444df
captianjroot <captianjroot@live.com>
2016-06-23 19:30:39 -0500

    Fingers crossed this will be it

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info[0]<3:
 
 setup(
     name="agutil",
-    version="0.1.0a",
+    version="0.1.1a",
     packages=["agutil", "agutil.bio"],
     entry_points={
         "console_scripts":[
