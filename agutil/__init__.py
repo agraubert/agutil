a0fe0226e61655c5bb1a1ace69815cddf84d44e3
captianjroot <captianjroot@live.com>
2016-06-24 18:26:45 -0500

    Reconfigured repo structure for easier imports

---
+++ agutil/__init__.py
@@ -0,0 +1,2 @@
+from .src.search_range import search_range
+from .src.status_bar import status_bar
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info[0]<3:
 setup(
     name="agutil",
     version="0.1.4a",
-    packages=["agutil", "agutil.bio"],
+    packages=["agutil", "agutil.bio", "agutil.src"],
     entry_points={
         "console_scripts":[
             "maf2bed = agutil.bio.maf2bed:main",
