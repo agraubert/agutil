dd49cb2619def4c0672412fd5b2dac6485509401
agraubert <captianjroot@live.com>
2016-06-27 11:52:10 -0500

    Bump version to 0.2.0a

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info[0]<3:
 
 setup(
     name="agutil",
-    version="0.1.4a",
+    version="0.2.0a",
     packages=["agutil", "agutil.bio", "agutil.src"],
     entry_points={
         "console_scripts":[
