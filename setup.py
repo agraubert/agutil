96bfe20edb93aafe5b6974c4b987154bdd0469a4
agraubert <captianjroot@live.com>
2016-06-30 13:46:12 -0500

    Changed development status to alpha

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
 
         'Intended Audience :: Developers',
         'Topic :: Software Development',
