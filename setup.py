519bb17e3b69351b520cc6bf2df4f1a0e5e7bd69
agraubert <captianjroot@live.com>
2016-06-30 13:36:05 -0500

    Restricted version to 3.3+
    shutil.get_terminal_size does not exist in 3.2 and causes failiure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 import sys
-if sys.version_info[0]<3:
+if sys.version_info<(3,3):
     print("This python version is not supported:")
     print(sys.version)
     print("agutil requires python 3.0 or greater")
@@ -35,7 +35,9 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
     ],
 
     author = "Aaron Graubert",
