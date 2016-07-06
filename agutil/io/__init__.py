6d1ac9f15e58679f5357bc397002682faf622818
agraubert <captianjroot@live.com>
2016-07-06 17:02:02 -0500

    Initial implimentation of socket class
    Added simple test case.  More robust case is needed
    
    Also add handling of timeouts and blocking/non-blocking
    
    Basically, Socket needs more complexity, but this is a good starting point

--- agutil/io/__init__.py
+++ agutil/io/__init__.py
@@ -1,1 +1,2 @@
 from .src.socket import Socket
+from .src.socket import SocketServer
---
+++ tests/test_socket.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+from py_compile import compile
+
+class test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.script_path = os.path.join(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.abspath(__file__)
+                )
+            ),
+            "agutil",
+            "io",
+            "src",
+            "socket.py"
+        )
+
+    def test_compilation(self):
+        compiled_path = compile(self.script_path)
+        self.assertTrue(compiled_path)
