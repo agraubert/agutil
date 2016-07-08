2cedcd79190983bff4feda409c688f1e97e8970f
agraubert <captianjroot@live.com>
2016-07-08 11:37:50 -0500

    Added markers for queued sockets

--- agutil/security/src/core.py
+++ agutil/security/src/core.py
@@ -5,7 +5,9 @@ def new(address, port, password=None):
     if address == 'listen' or address == '':
         #listen for connections
         ss = io.SocketServer(port, queue=0)
+        # s = channel.SecureSocket(io.QueuedSocket(ss.accept()), False, password)
         s = channel.SecureSocket(ss.accept(), False, password)
         ss.close()
         return s
+    # return channel.SecureSocket(io.QueuedSocket(io.Socket(address, port)), True, password)
     return channel.SecureSocket(io.Socket(address, port), True, password)
