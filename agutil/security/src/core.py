caebbc47565297fd489ac7d8f21a6e83a533c813
agraubert <captianjroot@live.com>
2016-07-08 11:20:54 -0500

    initial work on security module
    not finished

---
+++ agutil/security/src/core.py
@@ -0,0 +1,11 @@
+import ..io
+import .channel
+
+def new(address, port, password=None):
+    if address == 'listen' or address == '':
+        #listen for connections
+        ss = io.SocketServer(port, queue=0)
+        s = channel.SecureSocket(ss.accept(), False, password)
+        ss.close()
+        return s
+    return channel.SecureSocket(io.Socket(address, port), True, password)
