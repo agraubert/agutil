b3ef687ca4ac999b2585cc2bcd4e79405d090732
agraubert <captianjroot@live.com>
2016-07-15 11:29:42 -0500

    Added some notes to plan out the class

--- agutil/security/src/server.py
+++ agutil/security/src/server.py
@@ -16,9 +16,22 @@ class SecureServer:
         else:
             self.sock = SecureSocket(address, password, rsabits, verbose, timeout)
         self.tasks = {}
+        self.authqueue = {}
+        self.schedulinglock = threading.Condition()
+        self._shutdown = False
+        self._listener = None #Constantly receives from __cmd__ and adds new tasks to the scheduling queue
+        self._scheduler = None #Pops new commands out of the scheduling queue, and spawns a new thread to deal with each task
+        #Commands which require pre-authorization to start (ie: file transfer) are put in a holding queue instead of being scheduled
+        #Once the user authorizes the command (ie: .savefile()) the task is scheduled properly
 
     def _reserve_task(self, prefix):
         taskname = prefix+"_"+"".join(str(random.randint(0,9)) for _ in range(3))
         while taskname in self.tasks:
             taskname = prefix+"_"+"".join(str(random.randint(0,9)) for _ in range(3))
         return taskname
+
+    def shutdown(self):
+        if self._shutdown:
+            return
+        self.schedulinglock.acquire()
+        self._shutdown = True
