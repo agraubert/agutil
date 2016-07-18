8e090395714d2ab8561907e3022a173c556ef87c
agraubert <captianjroot@live.com>
2016-07-18 16:32:08 -0500

    Added SecureServer entry to security module __init__

--- agutil/security/__init__.py
+++ agutil/security/__init__.py
@@ -1,3 +1,4 @@
 _PROTOCOL_VERSION_ = '2.0.0'
 _PROTOCOL_IDENTIFIER_ = "<Agutil-security> <v%s>"%(_PROTOCOL_VERSION_)
 from .src.securesocket import SecureSocket
+from .src.server import SecureServer
