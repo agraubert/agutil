5466d70cd297fe94a5ffd5f7b282b4d7b95a8daf
agraubert <captianjroot@live.com>
2016-08-23 16:42:51 -0500

    Dumb Protocol typo

--- agutil/io/__init__.py
+++ agutil/io/__init__.py
@@ -1,4 +1,4 @@
-_PROTOCOL_IDENTIFIER_ = "<agutil><__protocol:1.0.0>"
+_PROTOCOL_IDENTIFIER_ = "<agutil><__protocol__:1.0.0>"
 from .src.socket import Socket
 from .src.socket import SocketServer
 from .src.queuedsocket import QueuedSocket
