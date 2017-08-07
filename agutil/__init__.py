50439f2addba9bcb128d69504db8df767cf0cf91
Aaron Graubert <captianjroot@live.com>
2017-08-07 17:18:32 -0400

    main __init__ file and maf2bed.py now pep8 compliant

--- agutil/__init__.py
+++ agutil/__init__.py
@@ -1,5 +1,12 @@
 from .src.search_range import search_range
 from .src.status_bar import status_bar
 from .src.logger import Logger, DummyLog
-from .src.misc import split_iterable, byte_xor, intToBytes, bytesToInt, hashfile, byteSize
+from .src.misc import (
+    split_iterable,
+    byte_xor,
+    intToBytes,
+    bytesToInt,
+    hashfile,
+    byteSize
+)
 from .src.shell import cmd, StdOutAdapter, ShellReturnObject
