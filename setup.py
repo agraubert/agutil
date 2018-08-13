1b610c93533f558367def01ed580a5256f8a27de
Aaron Graubert <captianjroot@live.com>
2018-08-13 14:25:26 -0400

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -73,7 +73,9 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only"
     ],
 
     author = "Aaron Graubert",
