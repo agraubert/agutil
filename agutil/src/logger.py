c4d0102fa6321b7278ef2a37f83037deaff2bb11
Aaron Graubert <captianjroot@live.com>
2018-08-21 15:33:01 -0400

    Updated logger

--- agutil/src/logger.py
+++ agutil/src/logger.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import sys
+import functools
 # Timestamp format: "[%a %m/%d/%Y %I:%M:%S %p]"
 
 
@@ -20,39 +21,21 @@ DummyLog.bindToSender = _bindToSender
 DummyLog.name = ""
 DummyLog.close = _close
 
-class object:
-    def __init__(self, val):
-        self.val = val
-
-    def __le__(self, other):
-        print("GETTING:", self.val, other)
-        return False
-
-    def __ge__(self, other):
-        print("GETTING:", self.val, other)
-        return False
-
-    def __len__(self):
-        return len(self.val)
-
-    def __getitem__(self, idx):
-        return self.val[idx]
-
 class Logger(object):
     LOGLEVEL_NONE = logging.CRITICAL
     LOGLEVEL_ERROR = logging.ERROR
     LOGLEVEL_WARN = logging.WARN
     LOGLEVEL_INFO = logging.INFO
     LOGLEVEL_DEBUG = logging.DEBUG
-    LOGLEVEL_DETAIL = logging.DEBUG
+    LOGLEVEL_DETAIL = 5
     def __init__(
         self,
-        name='agutil',
         filename=None,
+        name='agutil',
         loglevel=logging.INFO
     ):
-        self.logger = logging.getLogger('agutil')
-        self.logger.setLevel(1)
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(loglevel)
         if filename is None:
             self.handler = logging.StreamHandler(sys.stdout)
         elif isinstance(filename, str):
@@ -65,26 +48,38 @@ class Logger(object):
                 '%a %m/%d/%Y %I:%M:%S %p'
             )
         )
-        self.handler.createLock()
-        self.raw_message('[LOG STARTED]')
-        self.collection = {
-            'ERROR': [],
-            'DEBUG': []
+        # self.handler.setLevel(loglevel)
+        self.logger.addHandler(self.handler)
+
+        self.collection = {}
+        self.channel_defs = {
+            'DEBUG': logging.DEBUG,
+            'DETAIL': 5,
+            'INFO': logging.INFO,
+            'WARNING': logging.WARN,
+            'ERROR': logging.ERROR
         }
         self.mutes = {}
+        logging.addLevelName(5, 'DETAIL')
+        if loglevel <= logging.ERROR:
+            self.setChannelCollection('ERROR', True)
+        if loglevel <= logging.WARNING:
+            self.setChannelCollection("WARNING", True)
+        self.name = name
+        self.log('[LOG STARTED]', 'LOGGER')
+
+    def _forward_warn(self, channel):
+        return (
+            'WARNING'
+            if channel == 'WARN' and 'WARN' not in self.channel_defs
+            else channel
+        )
+
+    def __call__(self, message, sender='ANONYMOUS', channel='INFO'):
+        return self.log(message, sender, channel)
 
     def log(self, message, sender='ANONYMOUS', channel='INFO'):
-        channel = {
-            'DEBUG': logging.DEBUG,
-            'DETAIL': logging.DEBUG,
-            'INFO': logging.INFO,
-            'WARN': logging.WARN,
-            'ERROR': logging.ERROR
-        }[channel]
-        self.logger.log(
-            channel,
-            '[%s] : %s' % (sender, message)
-        )
+        channel = self._forward_warn(channel)
         if channel in self.collection:
             self.handler.acquire()
             try:
@@ -98,33 +93,61 @@ class Logger(object):
                 )
             finally:
                 self.handler.release()
+        if sender not in self.mutes:
+            try:
+                self.logger.log(
+                    self.channel_defs[channel],
+                    '[%s] : %s' % (sender, message)
+                )
+            except KeyError as e:
+                self.log(
+                    "Sender [%s] attempted to use undefined channel [%s]" % (
+                        sender, channel
+                    ),
+                    'LOGGER',
+                    'WARN'
+                )
+        else:
+            self.mutes[sender].append((message, channel))
 
-    def raw_message(self, msg):
-        self.logger.handle(
+    def _raw_message(self, msg, ch=20):
+        self.logger.callHandlers(
             self.logger.makeRecord(
                 self.logger.name,
-                20,
-                '20',
-                20,
+                ch,
+                '<NONE>',
+                ch,
                 msg,
-                (msg,),
+                (),
                 None
             )
         )
 
+    def _set_level(self, l):
+        self.logger.setLevel(l)
+
+    def _get_level(self, l):
+        return self.logger.getEffectiveLevel()
+
+    level = property(_get_level, _set_level)
+
+    def addChannel(self, name, priority):
+        self.channel_defs[name] = priority
+        logging.addLevelName(priority, name)
+        if priority >= logging.WARNING and self.logger.isEnabledFor(priority):
+            self.setChannelCollection(name, True)
+
     def bindToSender(self, sender, can_close=True):
-
-        def output(message, channel="INFO"):
-            return self.log(message, sender, channel)
-
-        def _bindToSender(sender):
-            return self.bindToSender(sender, False)
-        output.bindToSender = _bindToSender
+        output = functools.partial(self.log, sender=sender)
+        output.bindToSender = functools.partial(self.bindToSender, can_close=False)
         output.name = sender
         output.close = self.close if can_close else lambda: None
         return output
 
-    def setChannelCollection(self, channel, collect):
+    def setChannelCollection(self, channel, collect=True):
+        channel = self._forward_warn(channel)
+        if channel not in self.channel_defs:
+            raise NameError("No such channel '%s'" % channel)
         self.handler.acquire()
         try:
             if collect and channel not in self.collection:
@@ -138,18 +161,15 @@ class Logger(object):
         if mutee not in self.mutes:
             self.mutes[mutee] = []
             self.log(
-                "Sender [%s] has been muted by [%s]" % (
-                    mutee,
-                    muter
-                ),
-                sender="LOGGER"
+                "Muting sender [%s]" % mutee,
+                muter
             )
 
     def unmute(self, mutee):
         if mutee in self.mutes:
             self.log("Sender [%s] has been unmuted" % mutee, sender="LOGGER")
             if len(self.mutes[mutee]):
-                (message, channel) = self.mutes[mutee][0]
+                (message, channel) = self.mutes[mutee][-1]
                 suppressed = len(self.mutes[mutee])-1
                 del self.mutes[mutee]
                 self.log(
@@ -160,191 +180,39 @@ class Logger(object):
                     mutee,
                     channel
                 )
+            else:
+                del self.mutes[mutee]
+
 
     def close(self):
-        self.raw_message(">>>>>Logger shutting down")
+        self.log("Logger shutting down", 'LOGGER')
+        self.handler.setFormatter(
+            logging.Formatter('%(message)s')
+        )
         for channel in sorted(self.collection):
-            if len(self.collection[channel]):
-                self.raw_message(">>>>>Dump of channel "+channel)
+            enabled = self.logger.isEnabledFor(self.channel_defs[channel])
+            if len(self.collection[channel]) and enabled:
+                self._raw_message(
+                    "<Dump of channel "+channel+'>',
+                    self.channel_defs[channel]
+                )
                 for line in self.collection[channel]:
-                    self.raw_message(line)
-                self.raw_message('--------------------')
-            self.log('---[LOG STOPPED]---', 'LOGGER', 'INFO')
+                    self._raw_message(line, self.channel_defs[channel])
+                self._raw_message(
+                    '--------------------',
+                    self.channel_defs[channel]
+                )
+        self._raw_message(
+            '[%s] [%s] [%s] : %s' % (
+                time.strftime("%a %m/%d/%Y %I:%M:%S %p"),
+                'INFO',
+                'LOGGER',
+                '[LOG STOPPED]'
+            )
+        )
         self.handler.acquire()
         try:
             self.logger.removeHandler(self.handler)
             self.handler.close()
         finally:
             self.handler.release()
-
-# class Logger(object):
-#
-#
-#     def __init__(
-#         self,
-#         filename,
-#         header="Agutil Logger",
-#         log_level=LOGLEVEL_INFO,
-#         stdout_level=LOGLEVEL_NONE
-#     ):
-#         self.logger = logging.getLogger('agutil')
-#         self.logger.setLevel(1)
-#         self.filelog = (
-#             logging.FileHandler(filename) if filename is not None
-#             else None
-#         )
-#         if self.filelog is not None:
-#             self.filelog.setFormatter(
-#                 logging.Formatter('%(message)s')
-#             )
-#             self.logger.addHandler(self.filelog)
-#         self.outlog = logging.StreamHandler(sys.stdout)
-#         self.outlog.setFormatter(logging.Formatter('%(message)s'))
-#         self.logger.addHandler(self.outlog)
-#         self.channels = {
-#             'ERROR': [
-#                 Logger.LOGLEVEL_ERROR >= log_level,
-#                 Logger.LOGLEVEL_ERROR >= stdout_level,
-#                 True
-#             ],
-#             'WARN': [
-#                 Logger.LOGLEVEL_WARN >= log_level,
-#                 Logger.LOGLEVEL_WARN >= stdout_level,
-#                 True
-#             ],
-#             'INFO': [
-#                 Logger.LOGLEVEL_INFO >= log_level,
-#                 Logger.LOGLEVEL_INFO >= stdout_level,
-#                 False
-#             ],
-#             'DEBUG': [
-#                 Logger.LOGLEVEL_DEBUG >= log_level,
-#                 Logger.LOGLEVEL_DEBUG >= stdout_level,
-#                 False
-#             ],
-#             'DETAIL': [
-#                 Logger.LOGLEVEL_DETAIL >= log_level,
-#                 Logger.LOGLEVEL_DETAIL >= stdout_level,
-#                 False
-#             ]
-#         }
-#         self._shutdown = False
-#         self.mutes = {}
-#         self.collection = {
-#             "ERROR": [],
-#             "WARN": []
-#         }
-#
-#         self._idle = False
-#         if self.filelog is not None:
-#             self.filelog.emit(
-#                 logging.LogRecord(message='---[LOG STARTED]---')
-#             )
-#             self.filelog.emit(
-#                 logging.LogRecord(message=header+time.strftime("  <%a %m/%d/%Y %I:%M:%S %p>"))
-#             )
-#
-#     def log(self, message, sender="ANONYMOUS", channel="INFO"):
-#         if not self._shutdown:
-#             if sender not in self.mutes:
-#                 formatted = "[%s] [%s] [%s] : %s" % (
-#                     time.strftime("%a %m/%d/%Y %I:%M:%S %p"),
-#                     channel,
-#                     sender,
-#                     message
-#                 )
-#                 print("Logging message:", formatted)
-#                 self.logger.log(
-#                     logging.INFO,
-#                     formatted
-#                 )
-#                 if self.channels[channel][2]:
-#                     self.collection[channel].append(formatted)
-#             else:
-#                 self.mutes[sender].append((message, channel))
-#
-#     def close(self):
-#         if self._shutdown:
-#             return
-#         self.log(">>>>>Logger shutting down", sender="LOGGER", channel="INFO")
-#         if self.filelog is not None:
-#             for channel in sorted(self.collection):
-#                 if len(self.collection[channel]):
-#                     self.filelog.emit(
-#                         logging.LogRecord(message=">>>>>Dump of channel "+channel)
-#                     )
-#                     for line in self.collection[channel]:
-#                         self.filelog.emit(
-#                             logging.LogRecord(message=line)
-#                         )
-#                     self.filelog.emit(
-#                         logging.LogRecord(message='--------------------')
-#                     )
-#             self.log('---[LOG STOPPED]---', 'LOGGER', 'INFO')
-#         self._shutdown = True
-#         self.outlog.acquire()
-#         try:
-#             self.logger.removeHandler(self.outlog)
-#             self.outlog.close()
-#         finally:
-#             self.outlog.release()
-#         if self.filelog is not None:
-#             self.filelog.acquire()
-#             try:
-#                 self.logger.remove(self.filelog)
-#                 self.filelog.close()
-#             finally:
-#                 self.filelog.release()
-#
-#     def bindToSender(self, sender, can_close=True):
-#
-#         def output(message, channel="INFO"):
-#             return self.log(message, sender, channel)
-#
-#         def _bindToSender(sender):
-#             return self.bindToSender(sender, False)
-#         output.bindToSender = _bindToSender
-#         output.name = sender
-#         output.close = self.close if can_close else lambda: None
-#         return output
-#
-#     def setChannelFilters(self, channel, log, display):
-#         collect = False if channel not in self.channels else (
-#             self.channels[channel][2]
-#         )
-#         self.channels[channel] = [bool(log), bool(display), collect]
-#
-#     def setChannelCollection(self, channel, collect):
-#         if channel in self.channels:
-#             self.channels[channel][2] = bool(collect)
-#             if collect and channel not in self.collection:
-#                 self.collection[channel] = []
-#             elif channel in self.collection and not collect:
-#                 del self.collection[channel]
-#
-#     def mute(self, mutee, muter="ANONYMOUS"):
-#         if mutee not in self.mutes:
-#             self.mutes[mutee] = []
-#             self.log(
-#                 "Sender [%s] has been muted by [%s]" % (
-#                     mutee,
-#                     muter
-#                 ),
-#                 sender="LOGGER"
-#             )
-#
-#     def unmute(self, mutee):
-#         if mutee in self.mutes:
-#             self.log("Sender [%s] has been unmuted" % mutee, sender="LOGGER")
-#             if len(self.mutes[mutee]):
-#                 (message, channel) = self.mutes[mutee][0]
-#                 suppressed = len(self.mutes[mutee])-1
-#                 del self.mutes[mutee]
-#                 self.log(
-#                     "%s <An additional %d messages were suppressed>" % (
-#                         message,
-#                         suppressed
-#                     ),
-#                     mutee,
-#                     channel
-#                 )
